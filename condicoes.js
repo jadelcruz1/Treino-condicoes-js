dac61ba2151ef0cc33acb4705879f5fae24162d9
jardelcruz1 <jardelcruz@gmail.com>
2021-09-28 17:15:28 -0300

    condicoes simples

---
+++ condicoes.js
@@ -0,0 +1,7 @@
+var vel = 55
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida.Multa`)
+}
+
+console.log(`Dirija sempre usando cinto e seguranca`)
